6c86025d78e6d5db9d419961bb3121088d65e71e
夏浩文 <835913166@qq.com>
2019-09-30 17:42:15 +0800

    Ivan
    * done and done
    * add example.gif

---
+++ WaterHoverLayout/WaterHoverLayout/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  WaterHoverLayout
+//
+//  Created by mac on 2019/9/30.
+//  Copyright © 2019 com.ivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
+@interface UIView (Extra)
+
+- (void)removeAllSubViews;
+
+@end
---
+++ WaterHoverLayout/WaterHoverLayout/Views/WaterCollectionViewCell.h
@@ -0,0 +1,19 @@
+//
+//  WaterCollectionViewCell.h
+//  WaterHoverLayout
+//
+//  Created by mac on 2019/9/30.
+//  Copyright © 2019 com.ivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WaterCollectionViewCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *textLabel;
+
+@end
+
+NS_ASSUME_NONNULL_END
